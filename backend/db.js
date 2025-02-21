e0dc96d5075924f09b01445b6be748b0982bb2aa
raghav-0205 <raghavaman0502@gmail.com>
2025-02-21 10:18:07 +0530

    adding db

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import { createConnection } from "mysql2";
 const db = createConnection({
     host: process.env.DB_HOST || "127.0.0.1",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "tanuj726",
+    password: process.env.DB_PASSWORD || "050201",
     database: process.env.DB_NAME || "netflix",
 });
 
